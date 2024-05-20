9c85a28732249ebfddef69fb4a2de60f2b1ba6b5
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-05-19 21:33:00 -0500

    Update api_sql.py

--- api_sql.py
+++ api_sql.py
@@ -1,2 +1,15 @@
 import requests
 import pyodbc
+
+
+# URL de la API
+api_url = "http://localhost:8080/api"
+# Realizar la solicitud GET a la API
+response = requests.get(api_url)
+
+# Verificar que la solicitud fue exitosa
+if response.status_code == 200:
+    data = response.json()  # Asumimos que la API devuelve un JSON
+else:
+    print(f"Error al acceder a la API: {response.status_code}")
+    exit()
